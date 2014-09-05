28dbcb763820c2a297f25d9c013f76fd3fdb782e
Peter Scott <peter_scott@apple.com>
2014-09-05 14:13:04 -0700

    Make header-file C++-compatible.

--- murmur3.h
+++ murmur3.h
@@ -8,6 +8,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 //-----------------------------------------------------------------------------
 
 void MurmurHash3_x86_32 (const void *key, int len, uint32_t seed, void *out);
@@ -17,5 +21,9 @@ void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, void *out);
 void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
 
 //-----------------------------------------------------------------------------
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _MURMURHASH3_H_
